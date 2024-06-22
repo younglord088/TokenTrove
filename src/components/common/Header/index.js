5694b24de4c5e1eeb9cea3cc019b4627ece0d2e0
Yash Panjwani <yashmpanjwani@gmail.com>
2024-06-22 23:27:32 +0530

    Create Navbar for  Header

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./styles.css"
+
 function Header() {
-  return (
-    <div>header</div>
-  )
+  return <div className='navbar'>
+    <h1 className='logo'>TokenTrove<span style={{color:"var(--blue)"}}>.</span></h1>
+    <div className='links'>
+      <a href='#'><p className='link '>Home</p></a>
+      <a href='#'><p className='link '>Compare</p></a>
+      <a href='#'><p className='link '>Watchlist</p></a>
+      <a href='#'><p className='link '>Dashboard</p></a>
+    </div>
+  </div>
+  
 }
 
 export default Header
