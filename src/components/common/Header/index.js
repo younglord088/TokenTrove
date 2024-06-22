5386aa6cf0ea62e6ca18b790eeaba322942e9648
Yash Panjwani <yashmpanjwani@gmail.com>
2024-06-23 04:18:24 +0530

    Add @mui/icons-material, update paths, components, and styles

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./styles.css"
+import Button from "../Button";
+import TemporaryDrawer from "src/components/Common/Header/drawer.js";
+import "./styles.css";
 
-function Header() {
+
+function Header(){
   return <div className='navbar'>
     <h1 className='logo'>TokenTrove<span style={{color:"var(--blue)"}}>.</span></h1>
     <div className='links'>
       <a href='#'><p className='link '>Home</p></a>
       <a href='#'><p className='link '>Compare</p></a>
       <a href='#'><p className='link '>Watchlist</p></a>
-      <a href='#'><p className='link '>Dashboard</p></a>
+      <a href='#'>
+      <Button
+       text={"Dashboard"} 
+       
+       onClick={()=>{console.log('Dashboard Clicked')}}
+      /> </a> 
+      
+    </div>
+    <div className='mobile-drawer'>
+    <TemporaryDrawer/>
     </div>
   </div>
   
